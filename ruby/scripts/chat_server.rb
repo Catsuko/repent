9eb41b401cf578fe83f8df7f51cd7659d00fe864
Lewis Reid <lewis@nanameue.jp>
2023-10-03 23:36:14 +0900

    stop server gracefully

--- ruby/scripts/chat_server.rb
+++ ruby/scripts/chat_server.rb
@@ -4,4 +4,4 @@ require "grpc"
 server = GRPC::RpcServer.new
 server.add_http2_port("0.0.0.0:50051", :this_port_is_insecure)
 server.handle(Repent::Server)
-server.run_till_terminated
+server.run_till_terminated_or_interrupted([1, 'int', 'SIGQUIT'])
